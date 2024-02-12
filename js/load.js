d3f124685517c4c4e26b5af6e59c1d73b531e472
Ricardo Garcia <rg8607619@gmail.com>
2024-02-11 22:49:24 -0600

    Mejor galeria con una nueva libreria llamada Fonts(se creo una nueva carpeta)

--- js/load.js
+++ js/load.js
@@ -25,3 +25,15 @@ window.addEventListener("load", function () {
     }, delayTime);
   
 });
+
+
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference){
+    fulImgBox.style.display = "flex";
+    fulImg.src = reference
+}
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
